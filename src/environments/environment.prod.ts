161c20eba695603c2c52e145174b9ab01ab4e37e
Antonio Diaz <j.a.diaz@accenture.com>
2023-10-26 10:46:33 -0600

    fix(config): add prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    security: {
+      apiKey: 'c81084b228077ab3616868d169784295',
+      apiUrl: 'https://v3.football.api-sports.io/'
+    }
+  }
 };
